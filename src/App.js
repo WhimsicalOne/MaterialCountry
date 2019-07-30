b940c24e136b10f5906716f2fe95553572d41ad4
WhimsicalOne <dean@local-design.co.za>
2019-07-30 08:41:56 +0200

    Thingy

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="App">
+        <div className='App'>
             <h1>Hello World!</h1>
             <p>Hello World in a paragraph!</p>
+            This is sparta!
         </div>
     );
 }
